b2cc6fbb7ba1e1de974b0452242b92abdd42e23e
Harry Moreno <morenoh149@gmail.com>
2014-11-09 15:16:03 -0800

    added sidebar

--- public/app.js
+++ public/app.js
@@ -1,12 +1,3 @@
 $(function() {
   console.log('hey');
-
-  $('#myAffix').affix({
-    offset: {
-      top: 100,
-      bottom: function () {
-        return (this.bottom = $('.footer').outerHeight(true))
-      }
-    }
-  })
 });
