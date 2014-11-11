9154df5bdf3fb0ca0b9eb47af9c3fc4828ca07b1
Harry Moreno <morenoh149@gmail.com>
2014-11-11 01:10:46 -0800

    properly appending new images to main upon reaching bottom

--- public/app.js
+++ public/app.js
@@ -1,3 +1,38 @@
+/* Globals
+ */
+    // window.scrollTo(0, 1000);
+
+/* run javascript code while scrolling
+ * responsible for detecting end of content and loading more
+ */
+window.onscroll = function (ev) {
+  // var docHeight = document.body.offsetHeight;
+  // docHeight = docHeight === undefined ? window.document.documentElement.scrollHeight : docHeight;
+  var docHeight = document.getElementById('main').offsetHeight;
+
+  var winheight = window.innerHeight;
+  winheight = winheight === undefined ? document.documentElement.clientHeight : winheight;
+
+  var scrollpoint = window.scrollY;
+  scrollpoint = scrollpoint === undefined ? window.document.documentElement.scrollTop : scrollpoint;
+
+  if ((scrollpoint + winheight) >= docHeight) {
+    main.innerHTML += nextImageHTML();
+    window.setTimeout(function() {
+    }, 3000);
+  }
+};
+
+/* generates next 10 anchor+imageTags for injecting into the dom
+ */
+function nextImageHTML() {
+  return '<a id="part1-4">Part-1-Page4</a>' +
+         '<img src="http://swartzfiles.org/foia-request-001-page-004.jpg">';
+}
+
+/* JQuery padded code
+ */
 $(function() {
-  console.log('hey');
+  var main = document.getElementById('main');
+  console.log(main);
 });
