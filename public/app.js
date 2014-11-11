34a4ebb648d4fbec3dcf30f7b7aab016d3ce6655
Harry Moreno <morenoh149@gmail.com>
2014-11-11 02:04:27 -0800

    something working

--- public/app.js
+++ public/app.js
@@ -1,7 +1,12 @@
 /* Globals
  */
 // window.scrollTo(0, 1000);
+// window.scrollBy(0, 1000);
 var scrollFactor = 10;
+var ImageHeight = 1035;
+var FontHeight = 16;
+var HeaderHeight = 39;
+var topScrollCorrection = ((FontHeight + 2 + ImageHeight) * scrollFactor) - HeaderHeight - 300;
 
 /* run javascript code while scrolling
  * responsible for detecting end of content and loading more
@@ -17,10 +22,20 @@ window.onscroll = function (ev) {
   var scrollpoint = window.scrollY;
   scrollpoint = scrollpoint === undefined ? window.document.documentElement.scrollTop : scrollpoint;
 
+  // run on scroll bottom
   if ((scrollpoint + winheight) >= docHeight) {
     main.innerHTML += nextImageHTML(scrollFactor);
     window.setTimeout(function() {
     }, 3000);
+  }
+
+  // run on scroll top
+  if (scrollpoint === 0) {
+    main.innerHTML = nextImageHTML(scrollFactor) + main.innerHTML;
+    window.setTimeout(function() {
+      console.log('try to scroll correct');
+      window.scrollTo(0, topScrollCorrection);
+    }, 100);
   }
 };
 
